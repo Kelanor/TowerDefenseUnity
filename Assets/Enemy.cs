faa9c42355bd48e3fe9ec86dd872f972d21ad08c
Kelanor <triariy89@gmail.com>
2017-09-13 00:02:18 +0300

    Initial commit lesson 2

---
+++ Assets/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{   
+    public float speed;
+
+    private Transform target;
+    private int currentWaypointIndex = 0;
+
+    void Start()
+    {
+        target = WayPoints.points[0];
+    }
+    
+    void Update()
+    {
+        var direction = target.position - transform.position;
+        transform.Translate(direction.normalized * Time.deltaTime * speed, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) < 0.2f)
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    private void GetNextWaypoint()
+    {
+        if (currentWaypointIndex == WayPoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        currentWaypointIndex++;
+        target = WayPoints.points[currentWaypointIndex];
+    }
+}
