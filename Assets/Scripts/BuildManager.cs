5a1cd24a93f57e363fdb83f12f5e8eb017afb48f
Kelanor <triariy89@gmail.com>
2017-09-14 00:20:47 +0300

    lesson 6

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    #region Singleton
+    public static BuildManager instance;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("More than one BuildManager in scene!");
+            return;
+        }
+
+        instance = this;
+    }
+    #endregion
+
+    public GameObject standartTurretToBuild;
+
+    private GameObject turretToBuild;
+
+    void Start()
+    {
+        turretToBuild = standartTurretToBuild;
+    }
+
+    public GameObject GetTurretToBuild()
+    {
+        return turretToBuild;
+    }
+}
