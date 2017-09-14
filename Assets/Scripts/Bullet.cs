47a1ce83e2808b19cdff69e8ce2cda8bb592e814
n.gromilov <n.gromilov@vulkan.ru>
2017-09-14 12:45:35 +0300

    lesson 10

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -7,6 +7,8 @@ public class Bullet : MonoBehaviour
 {
     private Transform target;
     public float speed = 70f;
+    public float explosionRadius = 0;
+
     public GameObject impactEffect;
 
     public void Seek(Transform target)
@@ -32,14 +34,46 @@ public class Bullet : MonoBehaviour
         }
 
         transform.Translate(dir.normalized * distance, Space.World);
+        transform.LookAt(target);
     }
 
     private void HitTarget()
     {
         GameObject effectGO = Instantiate(impactEffect, transform.position, transform.rotation);
-        Destroy(effectGO, 2f);
+        Destroy(effectGO, 5f);
 
-        Destroy(target.gameObject);
+        if(explosionRadius > 0)
+        {
+            Explode();
+        }
+        else
+        {
+            Damage(target);
+        }
+
         Destroy(gameObject);
+    }
+
+    private void Explode()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
+        foreach (var collider in colliders)
+        {
+            if (collider.tag == "Enemy")
+            {
+                Damage(collider.transform);
+            }
+        }
+    }
+
+    void Damage(Transform enemy)
+    {
+        Destroy(enemy.gameObject);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
     }
 }
